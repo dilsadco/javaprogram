e4df1d48e83605fcdc328db546bc67aa32f67fcd
Dilsad <dilsadansari773@gmail.com>
2023-09-25 23:06:08 +0530

    Reverse Number

---
+++ ReversNumber.java
@@ -0,0 +1,19 @@
+package javaprogram;
+
+import java.util.Scanner;
+
+class ReversNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        sc.close();
+        int rev = 0;
+        int res = 0;
+        while (num > 0) {
+            res = num % 10;
+            num = num / 10;
+            rev = res + (rev * 10);
+        }
+        System.out.println(rev);
+    }
+}
